960c1b64f3d9c1b712ea9c9bb17155d60ebabdd9
Ace Logronio <64186285+aceeyou@users.noreply.github.com>
2023-07-23 22:16:31 +0800

    change scroll event listener to news-container

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -14,53 +14,53 @@ function News() {
 
     const newsContainer = document.querySelector(".news-container")
 
-    // newsContainer.addEventListener("wheel", (e) => {
-    //     e.preventDefault()
-    //     e.deltaY > 0 ? newsContainer.scrollLeft += 800 : newsContainer.scrollLeft -= 800
-    //     e.preventDefault()
-    // })
+    newsContainer.addEventListener("wheel", (e) => {
+        e.preventDefault()
+        e.deltaY > 0 ? newsContainer.scrollLeft += 800 : newsContainer.scrollLeft -= 800
+        e.preventDefault()
+    })
 
-    horizontalWheel(newsContainer)
+    // horizontalWheel(newsContainer)
 
-    function horizontalWheel(container) {
-        /** Max `scrollLeft` value */
-        let scrollWidth;
+    // function horizontalWheel(container) {
+    //     /** Max `scrollLeft` value */
+    //     let scrollWidth;
       
-        /** Desired scroll distance per animation frame */
-        let getScrollStep = () => scrollWidth / 2 /* ADJUST TO YOUR WISH */ ;
+    //     /** Desired scroll distance per animation frame */
+    //     let getScrollStep = () => scrollWidth / 2 /* ADJUST TO YOUR WISH */ ;
       
-        /** Target value for `scrollLeft` */
-        let targetLeft;
+    //     /** Target value for `scrollLeft` */
+    //     let targetLeft;
       
-        function scrollLeft() {
-          let beforeLeft = container.scrollLeft;
-          let wantDx = getScrollStep();
-          let diff = targetLeft - container.scrollLeft;
-          let dX = wantDx >= Math.abs(diff) ? diff : Math.sign(diff) * wantDx;
+    //     function scrollLeft() {
+    //       let beforeLeft = container.scrollLeft;
+    //       let wantDx = getScrollStep();
+    //       let diff = targetLeft - container.scrollLeft;
+    //       let dX = wantDx >= Math.abs(diff) ? diff : Math.sign(diff) * wantDx;
       
-          // Performing horizontal scroll
-          container.scrollBy(dX, 0);
+    //       // Performing horizontal scroll
+    //       container.scrollBy(dX, 0);
       
-          // Break if smaller `diff` instead of `wantDx` was used
-          if (dX === diff)
-            return;
+    //       // Break if smaller `diff` instead of `wantDx` was used
+    //       if (dX === diff)
+    //         return;
       
-          // Break if can't scroll anymore or target reached
-          if (beforeLeft === container.scrollLeft || container.scrollLeft === targetLeft)
-            return;
+    //       // Break if can't scroll anymore or target reached
+    //       if (beforeLeft === container.scrollLeft || container.scrollLeft === targetLeft)
+    //         return;
       
-          requestAnimationFrame(scrollLeft);
-        }
+    //       requestAnimationFrame(scrollLeft);
+    //     }
       
-        container.addEventListener('wheel', e => {
-          e.preventDefault();
+    //     container.addEventListener('wheel', e => {
+    //       e.preventDefault();
       
-          scrollWidth = container.scrollWidth - container.clientWidth;
-          targetLeft = Math.min((scrollWidth + 800), Math.max(0, container.scrollLeft + (e.deltaY * 5)));
+    //       scrollWidth = container.scrollWidth - container.clientWidth;
+    //       targetLeft = Math.min((scrollWidth + 800), Math.max(0, container.scrollLeft + (e.deltaY * 5)));
       
-          requestAnimationFrame(scrollLeft);
-        });
-      }
+    //       requestAnimationFrame(scrollLeft);
+    //     });
+    //   }
 
     const fetchNews = async () => {
         let countrySource = 'us'
