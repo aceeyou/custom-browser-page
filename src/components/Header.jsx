e16db234ac4fdef4fe934b9fbd133539bb86ee0a
Ace Logronio <64186285+aceeyou@users.noreply.github.com>
2023-07-28 21:50:25 +0800

    fix weather icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,61 +1,80 @@
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 import "../App.css";
-import "../default.css"
+import "../default.css";
 
 export default function Header() {
+  // Weather API Key = 1bdebbd5339f4cbe976110401231207
+  const [weather, setWeather] = useState({});
+  //   const [weatherTooltip, setWeatherTooltip] = useState(false);
 
-    // Weather API Key = 1bdebbd5339f4cbe976110401231207
-    const [weather, setWeather] = useState({})
-    const [weatherTooltip, setWeatherTooltip] = useState(false)
+  useEffect(() => {
+    fetchWeather();
+  });
 
-    useEffect(() => {
-        fetchWeather()
-    })
+  const MONTH = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const DAYS = [
+    "SUNDAY",
+    "MONDAY",
+    "TUESDAY",
+    "WEDNESDAY",
+    "THURSDAY",
+    "FRIDAY",
+    "SATURDAY",
+  ];
 
-    const MONTH = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const DAYS = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
+  const getDateNow = () => {
+    let dateNow = new Date();
+    return `${DAYS[dateNow.getDay()]}, ${
+      MONTH[dateNow.getMonth()]
+    } ${dateNow.getDate()}, `;
+  };
 
-    const getDateNow = () => {
-        let dateNow = new Date()
-        return `${DAYS[dateNow.getDay()]}, ${MONTH[dateNow.getMonth()]} ${dateNow.getDate()}, `
-    }
-
-    const fetchWeather = () => {
-        try {
-            fetch('https://api.weatherapi.com/v1/current.json?key=1bdebbd5339f4cbe976110401231207&q=Philippines&aqi=yes', {
-                method: "GET"
-            })
-            .then(res => res.json())
-            .then(data => {
-                setWeather(data.current)
-            })
-        } catch (error) {
-            console.log(error.message)
+  const fetchWeather = () => {
+    try {
+      fetch(
+        "https://api.weatherapi.com/v1/current.json?key=1bdebbd5339f4cbe976110401231207&q=Philippines&aqi=yes",
+        {
+          method: "GET",
         }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setWeather(data.current);
+        });
+    } catch (error) {
+      console.log(error.message);
     }
+  };
 
-    return (
-        <header className='App-Header'>
-            <h1 className='header-logo'>Ace Logronio</h1>
-            <div className='App-Header-Date-Weather'>
-                <p className='Date-Weather-Text'>{getDateNow()}</p>
-                <div className='weather-information'>
-                    <p className='weather-temperature'>{weather.temp_c}°C</p>
-                    {
-                        weather?.condition.icon ?
-                        <img className='weather-icon' src={`https${weather?.condition.icon}`} onMouseOver={() => setWeatherTooltip(true)} onMouseOut={() => setWeatherTooltip(false)} alt="current weather icon" />
-                        : null
-                    }
-                    {/* {
-                        weatherTooltip ? 
-                        (
-                            <div className='weather-tooltip'>
-                                <p>{weather.condition.text}</p>
-                            </div>
-                        ) : null
-                    } */}
-                </div>
-            </div>
-        </header>
-    )
+  return (
+    <header className="App-Header">
+      <h1 className="header-logo">Ace Logronio</h1>
+      <div className="App-Header-Date-Weather">
+        <p className="Date-Weather-Text">{getDateNow()}</p>
+        <div className="weather-information">
+          <p className="weather-temperature">{weather.temp_c}°C</p>
+          {weather?.condition?.icon ? (
+            <img
+              className="weather-icon"
+              src={`https${weather?.condition.icon}`}
+              alt="current weather icon"
+            />
+          ) : null}
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,91 +1,92 @@
-import React, {useEffect, useState} from 'react'
-import NewsItem from './NewsItem'
+import React, { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
 
 function News() {
+  // Load articles from API
+  // API KEY = 3290737503df4b2497708061f590d5f4
 
-    // Load articles from API
-    // API KEY = 3290737503df4b2497708061f590d5f4
+  const [news, setNews] = useState([]);
 
-    const [news, setNews] = useState([])
+  useEffect(() => {
+    fetchNews();
+  });
 
-    useEffect(() => {
-        fetchNews()
-    })
+  document.addEventListener("DOMContentLoaded", () => {
+    const newsContainer = document.querySelector(".news-container");
 
-    
-    document.addEventListener('DOMContentLoaded', () => {
-        const newsContainer = document.querySelector(".news-container")
-        
-        newsContainer.addEventListener("wheel", function(e) {
-            e.preventDefault()
-            e.deltaY > 0 ? newsContainer.scrollLeft += 800 : newsContainer.scrollLeft -= 800
-            e.preventDefault()
-        })
-    })
+    newsContainer.addEventListener("wheel", function (e) {
+      e.preventDefault();
+      e.deltaY > 0
+        ? (newsContainer.scrollLeft += 800)
+        : (newsContainer.scrollLeft -= 800);
+      e.preventDefault();
+    });
+  });
 
-    // horizontalWheel(newsContainer)
+  // horizontalWheel(newsContainer)
 
-    // function horizontalWheel(container) {
-    //     /** Max `scrollLeft` value */
-    //     let scrollWidth;
-      
-    //     /** Desired scroll distance per animation frame */
-    //     let getScrollStep = () => scrollWidth / 2 /* ADJUST TO YOUR WISH */ ;
-      
-    //     /** Target value for `scrollLeft` */
-    //     let targetLeft;
-      
-    //     function scrollLeft() {
-    //       let beforeLeft = container.scrollLeft;
-    //       let wantDx = getScrollStep();
-    //       let diff = targetLeft - container.scrollLeft;
-    //       let dX = wantDx >= Math.abs(diff) ? diff : Math.sign(diff) * wantDx;
-      
-    //       // Performing horizontal scroll
-    //       container.scrollBy(dX, 0);
-      
-    //       // Break if smaller `diff` instead of `wantDx` was used
-    //       if (dX === diff)
-    //         return;
-      
-    //       // Break if can't scroll anymore or target reached
-    //       if (beforeLeft === container.scrollLeft || container.scrollLeft === targetLeft)
-    //         return;
-      
-    //       requestAnimationFrame(scrollLeft);
-    //     }
-      
-    //     container.addEventListener('wheel', e => {
-    //       e.preventDefault();
-      
-    //       scrollWidth = container.scrollWidth - container.clientWidth;
-    //       targetLeft = Math.min((scrollWidth + 800), Math.max(0, container.scrollLeft + (e.deltaY * 5)));
-      
-    //       requestAnimationFrame(scrollLeft);
-    //     });
-    //   }
+  // function horizontalWheel(container) {
+  //     /** Max `scrollLeft` value */
+  //     let scrollWidth;
 
-    const fetchNews = async () => {
-        let countrySource = 'us'
-        try {
-            await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=3290737503df4b2497708061f590d5f4`)
-            .then(res => res.json())
-            .then(item => setNews(item.articles))
-        } catch (error) {
-            console.log(error.message)
-        }
-        // console.log(news)
+  //     /** Desired scroll distance per animation frame */
+  //     let getScrollStep = () => scrollWidth / 2 /* ADJUST TO YOUR WISH */ ;
+
+  //     /** Target value for `scrollLeft` */
+  //     let targetLeft;
+
+  //     function scrollLeft() {
+  //       let beforeLeft = container.scrollLeft;
+  //       let wantDx = getScrollStep();
+  //       let diff = targetLeft - container.scrollLeft;
+  //       let dX = wantDx >= Math.abs(diff) ? diff : Math.sign(diff) * wantDx;
+
+  //       // Performing horizontal scroll
+  //       container.scrollBy(dX, 0);
+
+  //       // Break if smaller `diff` instead of `wantDx` was used
+  //       if (dX === diff)
+  //         return;
+
+  //       // Break if can't scroll anymore or target reached
+  //       if (beforeLeft === container.scrollLeft || container.scrollLeft === targetLeft)
+  //         return;
+
+  //       requestAnimationFrame(scrollLeft);
+  //     }
+
+  //     container.addEventListener('wheel', e => {
+  //       e.preventDefault();
+
+  //       scrollWidth = container.scrollWidth - container.clientWidth;
+  //       targetLeft = Math.min((scrollWidth + 800), Math.max(0, container.scrollLeft + (e.deltaY * 5)));
+
+  //       requestAnimationFrame(scrollLeft);
+  //     });
+  //   }
+
+  const fetchNews = async () => {
+    let countrySource = "us";
+    try {
+      await fetch(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=3290737503df4b2497708061f590d5f4`
+      )
+        .then((res) => res.json())
+        .then((item) => setNews(item.articles));
+    } catch (error) {
+      console.log(error.message);
     }
+  };
   return (
-    <div className='discovery-panel'>
-        <h1 id="discovery-panel-title">Discover</h1>
-        <div className='news-container'>
-            {news?.map((article) => 
-                (<NewsItem news={article} />)
-            )}
-        </div>
+    <div className="discovery-panel">
+      <h1 id="discovery-panel-title">Discover</h1>
+      <div className="news-container">
+        {news?.map((article) => (
+          <NewsItem key={article.source.id} news={article} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default News
+export default News;
